12de0dd40ed4adc87c02666525fd066f729089ea
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-05-10 23:14:23 +0200

    rendering lists

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
 
 export default function Profile() {
+  const listItems = products.map((product) => (
+    <li key={product.id}>
+      {product.title}
+    </li>
+  ));
   return (
     <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
+      <ul>
+        {listItems}
+      </ul>
     </>
   );
 }
