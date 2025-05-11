237f551c33ef3e89dabc0f7fc24f406819b686cd
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-05-11 23:33:00 +0200

    update component to read the props from parent component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { useState } from 'react';
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <>
-      <MyButton />
-      <MyButton />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
 
     </>
   );
 }
 
 
-function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
+function MyButton({ count, onClick }) {
   return (
     <div>
-      <button onClick={handleClick}>
+      <button onClick={onClick}>
         Click me!
       </button>
-      <p>You clicked {count} time/s</p>
+      <p>You clicked {count} {count <= 1 ? "time" : "times"} </p>
     </div>
   );
 
